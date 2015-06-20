e2807592b1dbfb3fd866eed69654bb2d2168e570
Artur Ratajczak <artur.ratajczak1@gmail.com>
2015-06-20 12:04:59 +0200

    Init

---
+++ src/com/test/Test.java
@@ -0,0 +1,23 @@
+package com.test;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean (name = "hello")
+@SessionScoped
+public class Test implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String message;
+	
+	public String getMessage(){
+		return "Zwracam wiadomosc";
+	}
+
+}
